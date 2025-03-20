05ecf1ae7a993b9f10dceaa44a68089a50a9e0c3
Alan Harrison <alan@httcs.online>
2025-03-20 20:46:25 +0000

    fix _init__ to call setup routes

--- six-hack/app/__init__.py
+++ six-hack/app/__init__.py
@@ -1,9 +1,11 @@
+# filepath: /workspaces/six-hack/app/__init__.py
 from flask import Flask
+from .routes import setup_routes
 
 def create_app():
     app = Flask(__name__)
-    
+    app.config.from_object('config.Config')
+
     with app.app_context():
-        from . import routes
-        
-    return app
+        setup_routes(app)  # Register routes
+        return app
